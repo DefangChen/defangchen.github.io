34fd17eb64c4541f91f4c1090b716dae0ef4054b
DefangChen <34830084+DefangChen@users.noreply.github.com>
2025-09-23 20:36:03 +0000

    Deploying to gh-pages from @ DefangChen/defangchen.github.io@29ff54c4283eee13c0226cb59626124682a0f0ae 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "DefangChen/defchern.github.io",
+    "data-repo": "DefangChen/defangchen.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
